d3920a18f1a72d026c4eb98d59f8cbdf238cc48f
Roberto Carlos <betocarlos00@hotmail.com>
2021-12-18 00:45:34 -0300

    add identity e pequenas configurações
    adicionado identity, confirações em startup, _layout, migration e update

---
+++ UserCustom/UserCustom/Areas/Identity/Data/UserCustomUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace UserCustom.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the UserCustomUser class
+    public class UserCustomUser : IdentityUser
+    {
+    }
+}
---
+++ UserCustom/UserCustom/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserCustom.Areas.Identity.Data;
+using UserCustom.Data;
+
+[assembly: HostingStartup(typeof(UserCustom.Areas.Identity.IdentityHostingStartup))]
+namespace UserCustom.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserCustomContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserCustomContextConnection")));
+
+                services.AddDefaultIdentity<UserCustomUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserCustomContext>();
+            });
+        }
+    }
+}
